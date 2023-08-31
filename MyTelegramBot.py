e0dfec2b09e4431081a8d117d387fa17f7e1c7ee
Imanuelkarl <67210451+Imanuelkarl@users.noreply.github.com>
2023-08-31 18:49:31 +0100

    Create MyTelegramBot.py

---
+++ MyTelegramBot.py
@@ -0,0 +1,78 @@
+
+from typing import Final
+from telegram import Update
+from telegram.ext import Application,CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN : Final = "6564726550:AAE82aDJjmKm89M5SAoFDWdFHVnUPVrAd9c"
+Bot_Username :Final="@ImanuelAIBot"
+
+
+
+async def start_Command(update: Update,context : ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello and welcome to my AI")
+    
+    
+async def help_Command(update: Update,context : ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello and welcome to my AI")
+    
+    
+async def hello_Command(update: Update,context : ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello and welcome to my AI")
+    
+    
+    
+def handle_response(text : str):
+    
+    processed : str = text.lower()
+    if "hello" in processed:
+        return "Hi what can i do for you today"
+        
+    if "how are you" in processed:
+        return "I'm good and you"
+        
+    return "i do not understand you please"
+    
+    
+async def handle_image(update :Update, context :ContextTypes.DEFAULT_TYPE):
+    photo = update.message.photo[-1]  # Get the largest available photo
+    file_id = photo.file_id
+    await context.bot.send_photo(chat_id=update.message.chat_id, photo=photo.file_id)
+    
+async def handle_message(update : Update, context : ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+    
+    print(f'User ({update.message.chat.id}) in {message_type}: "{text}" ')
+    if message_type == "group":
+        if Bot_Username in text:
+            new_text:str =text.replace(Bot_Username," ").strip()
+            response: str = handle_response(new_text)
+        else :
+            return
+    else:
+        response: str = handle_response(text)
+        
+        
+    print("Bot:",response)
+    await update.message.reply_text(response)
+    
+    
+    
+async def error(update: Update,context : ContextTypes.DEFAULT_TYPE):
+    print(f' Update {update} caused error {context.error}')
+    
+    
+    
+if __name__ == "__main__":
+    app= Application.builder().token(TOKEN).build()
+    print("Starting...")
+    app.add_handler(CommandHandler("start",start_Command))
+    app.add_handler(CommandHandler("help",help_Command))
+    app.add_handler(CommandHandler("hello",hello_Command))
+    app.add_handler(MessageHandler(filters.TEXT,handle_message))
+    app.add_handler(MessageHandler(filters.PHOTO, handle_image))
+    app.add_error_handler(error)
+    
+    print("Polling...")
+    app.run_polling(poll_interval=3)
+    
